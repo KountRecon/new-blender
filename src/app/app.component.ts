a8583caaa0e549cf2bd038406fb4abf434d28942
KountAinte <drudgecode@gmail.com>
2018-08-12 07:17:25 -0700

    lazar commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'providence yeah !';
+  title = ' Welcome to Lazares Secret Gay Place !';
 }
